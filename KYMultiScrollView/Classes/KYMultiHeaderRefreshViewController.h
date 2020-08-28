271ffee1bdb46d279cc9729f1a71288e5ea1b047
燕雪峰 <ssi-yanxf@dfmc.com.cn>
2020-08-28 15:37:25 +0800

    交互处理

---
+++ KYMultiScrollView/Classes/KYMultiHeaderRefreshViewController.h
@@ -0,0 +1,16 @@
+//
+//  KYMultiHeaderRefreshViewController.h
+//  KYMultiScrollView
+//
+//  Created by yxf on 2020/8/28.
+//
+
+#import <KYMultiScrollView/KYMultiScrollView.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KYMultiHeaderRefreshViewController : KYMultiViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KYMultiScrollView/Classes/KYMultiHeaderRefreshViewController.m
@@ -0,0 +1,31 @@
+//
+//  KYMultiHeaderRefreshViewController.m
+//  KYMultiScrollView
+//
+//  Created by yxf on 2020/8/28.
+//
+
+#import "KYMultiHeaderRefreshViewController.h"
+
+@interface KYMultiHeaderRefreshViewController ()
+
+@end
+
+@implementation KYMultiHeaderRefreshViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
